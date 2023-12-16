d1b4a1cc7e89b228122fbb5e3fa1d86dd1d7bb80
mammadli <gulnar.mammadli@ut.ee>
2023-12-16 16:48:45 +0200

    Update signup, table and post creation

--- back/database.js
+++ back/database.js
@@ -1,39 +1,44 @@
-// database.js
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: "postgres",
-    password: "test12345",
-    database: "WAD-HW4",
-    host: "localhost",
-    port: "5432",
+  user: "postgres",
+  password: "postgres",
+  database: "WAD-HW4",
+  host: "localhost",
+  port: "5432",
 });
 
 const execute = async (query) => {
-    try {
-        await pool.connect(); // create a connection
-        await pool.query(query); // executes a query
-        return true;
-    } catch (error) {
-        console.error(error.stack);
-        return false;
-    }
+  try {
+    await pool.connect();
+    await pool.query(query);
+    return true;
+  } catch (error) {
+    console.error(error.stack);
+    return false;
+  }
 };
 
 const createTblQuery = `
     CREATE TABLE IF NOT EXISTS "users" (
         id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        username VARCHAR(200) NOT NULL UNIQUE,
         email VARCHAR(200) NOT NULL UNIQUE,
         password VARCHAR(200) NOT NULL 
     );
     CREATE TABLE IF NOT EXISTS "posts" (
 	    "id" SERIAL PRIMARY KEY,         
-	    "body" VARCHAR(200) NOT NULL
+	    "body" VARCHAR(200) NOT NULL,
+        "like_count" INT DEFAULT 0,
+        "comment_count" INT DEFAULT 0,
+        "created_time" TIMESTAMP NOT NULL,
+        "user_id" uuid NOT NULL,
+        CONSTRAINT "fk_user_id" FOREIGN KEY ("user_id") REFERENCES "users"("id")
     );`;
 
 execute(createTblQuery).then((result) => {
-    if (result) {
-        console.log('Tables "users", "posts" are created');
-    }
+  if (result) {
+    console.log('Tables "users", "posts" are created');
+  }
 });
 
 module.exports = pool;
--- back/server.js
+++ back/server.js
@@ -11,154 +11,164 @@ const app = express();
 
 app.use(cors({ origin: "http://localhost:8080", credentials: true }));
 
-app.use(express.json()); // Parses incoming requests with JSON payloads and is based on body-parser.
-app.use(cookieParser()); // Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
-
+app.use(express.json());
+app.use(cookieParser());
 const secret = "gdgdhdbcb770785rgdzqws";
-const maxAge = 60 * 60; //unlike cookies, the expiresIn in jwt token is calculated by seconds not milliseconds
-
+const maxAge = 60 * 60;
 const generateJWT = (id) => {
-    return jwt.sign({ id }, secret, { expiresIn: maxAge });
+  return jwt.sign({ id }, secret, { expiresIn: maxAge });
 };
 
 app.listen(port, () => {
-    console.log("Server is listening to port " + port);
+  console.log("Server is listening to port " + port);
 });
 
-// check user is authentication
 app.get("/auth/authenticate", async (req, res) => {
-    console.log("authentication request has been arrived");
-    const token = req.cookies.jwt; // assign the token named jwt to the token const
+  console.log("authentication request has been arrived");
+  const token = req.cookies.jwt;
 
-    let authenticated = false; // a user is not authenticated until proven the opposite
-    try {
-        if (token) {
-            //checks if the token exists
-
-            await jwt.verify(token, secret, (err) => {
-                //token exists, now we try to verify it
-                if (err) {
-                    // not verified, redirect to login page
-                    console.log(err.message);
-                    console.log("token is not verified");
-                    res.send({ authenticated: authenticated }); // authenticated = false
-                } else {
-                    // token exists and it is verified
-                    console.log("author is authinticated");
-                    authenticated = true;
-                    res.send({ authenticated: authenticated }); // authenticated = true
-                }
-            });
+  let authenticated = false;
+  try {
+    if (token) {
+      await jwt.verify(token, secret, (err) => {
+        if (err) {
+          console.log(err.message);
+          console.log("token is not verified");
+          return res.send({ authenticated: authenticated });
         } else {
-            //applies when the token does not exist
-            console.log("author is not authinticated");
-            res.send({ authenticated: authenticated }); // authenticated = false
+          console.log("author is authinticated");
+          authenticated = true;
+          return res.send({ authenticated: authenticated });
         }
-    } catch (err) {
-        console.error(err.message);
-        res.status(400).send(err.message);
+      });
+    } else {
+      console.log("author is not authinticated");
+      return res.send({ authenticated: authenticated });
     }
+  } catch (err) {
+    console.error(err.message);
+    return res.status(400).send(err.message);
+  }
 });
 
-// signup
 app.post("/auth/signup", async (req, res) => {
-    try {
-        console.log("a signup request has arrived");
-        //console.log(req.body);
-        const { email, password } = req.body;
+  try {
+    console.log("a signup request has arrived");
+    const { email, password, username } = req.body;
 
-        const salt = await bcrypt.genSalt(); //  generates the salt, i.e., a random string
-        const bcryptPassword = await bcrypt.hash(password, salt); // hash the password and the salt
-        const authUser = await pool.query(
-            // insert the user and the hashed password into the database
-            "INSERT INTO users(email, password) values ($1, $2) RETURNING*",
-            [email, bcryptPassword]
-        );
-        console.log(authUser.rows[0].id);
-        const token = await generateJWT(authUser.rows[0].id); // generates a JWT by taking the user id as an input (payload)
+    const salt = await bcrypt.genSalt();
+    const bcryptPassword = await bcrypt.hash(password, salt);
+    const authUser = await pool.query(
+      "INSERT INTO users(email, password, username) values ($1, $2, $3 ) RETURNING*",
+      [email, bcryptPassword, username]
+    );
+    console.log(authUser.rows[0].id);
+    const token = await generateJWT(authUser.rows[0].id);
 
-        res.status(201).cookie("jwt", token, { maxAge: 6000000, httpOnly: true }).json({ user_id: authUser.rows[0].id })
-            .send;
-    } catch (err) {
-        console.error(err.message);
-        res.status(400).send(err.message);
-    }
+    return res
+      .status(201)
+      .cookie("jwt", token, { maxAge: 6000000, httpOnly: true })
+      .json({ user_id: authUser.rows[0].id }).send;
+  } catch (err) {
+    console.error(err.message);
+    return res.status(400).send(err.message);
+  }
 });
 
-// login
 app.post("/auth/login", async (req, res) => {
-    try {
-        console.log("a login request has arrived");
-        const { email, password } = req.body;
-        const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
-        if (user.rows.length === 0) return res.status(401).json({ error: "User is not registered" });
+  try {
+    console.log("a login request has arrived");
+    const { email, password } = req.body;
+    const user = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (user.rows.length === 0)
+      return res.status(401).json({ error: "User is not registered" });
 
-        // check password
-        const validPassword = await bcrypt.compare(password, user.rows[0].password);
-        //console.log("validPassword:" + validPassword);
-        if (!validPassword) return res.status(401).json({ error: "Incorrect password" });
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
+    if (!validPassword)
+      return res.status(401).json({ error: "Incorrect password" });
 
-        const token = await generateJWT(user.rows[0].id);
-        res.status(201).cookie("jwt", token, { maxAge: 6000000, httpOnly: true }).json({ user_id: user.rows[0].id })
-            .send;
-    } catch (error) {
-        res.status(401).json({ error: error.message });
-    }
+    const token = await generateJWT(user.rows[0].id);
+    return res
+      .status(201)
+      .cookie("jwt", token, { maxAge: 6000000, httpOnly: true })
+      .json({ user_id: user.rows[0].id }).send;
+  } catch (error) {
+    return res.status(401).json({ error: error.message });
+  }
 });
 
-//logout a user = deletes the jwt
 app.get("/auth/logout", (req, res) => {
-    console.log("delete jwt request arrived");
-    res.status(202).clearCookie("jwt").json({ Msg: "cookie cleared" }).send;
+  console.log("delete jwt request arrived");
+  return res.status(202).clearCookie("jwt").json({ Msg: "cookie cleared" })
+    .send;
 });
 
-// post request
 app.post("/posts", async (req, res) => {
-    try {
-        console.log("a post request has arrived");
-        const post = req.body;
-        const newpost = await pool.query("INSERT INTO posts(body) values ($1) RETURNING*", [post.body]);
-        res.json(newpost);
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    console.log("a post request has arrived");
+    const { body, user_id } = req.body;
+    const created_time = new Date();
+    const formattedCreatedTime = created_time
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " ");
+
+    const name = await pool.query("SELECT username FROM users WHERE id = $1", [
+      user_id,
+    ]);
+    const username = name.rows[0].username;
+
+    const newpost = await pool.query(
+      "INSERT INTO posts(body, user_id, created_time) values ($1, $2, $3) RETURNING*",
+      [body, user_id, formattedCreatedTime]
+    );
+
+    const post = newpost.rows[0];
+    console.log("new post", newpost);
+    return res.status(201).json({ post: { ...post, username } }).send;
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ err: err.message }).send;
+  }
 });
 
-// get posts
 app.get("/posts", async (req, res) => {
-    try {
-        console.log("get posts request has arrived");
-        const posts = await pool.query("SELECT * FROM posts");
-        res.json(posts.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    console.log("get posts request has arrived");
+    const posts = await pool.query("SELECT * FROM posts");
+    return res.json(posts.rows);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ err: err.message }).send;
+  }
 });
 
-// get post
 app.get("/posts/:id", async (req, res) => {
-    try {
-        console.log("get a post with route parameter  request has arrived");
-        const { id } = req.params; // assigning all route "parameters" to the id "object"
-        const posts = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
-        res.json(posts.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    console.log("get a post with route parameter  request has arrived");
+    const { id } = req.params;
+    const posts = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
+    return res.json(posts.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ err: err.message }).send;
+  }
 });
 
-// update post
 app.put("/posts/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const post = req.body;
-        console.log("update request has arrived");
-        const updatepost = await pool.query("UPDATE posts SET (title, body, urllink) = ($2) WHERE id = $1", [
-            id,
-            post.body,
-        ]);
-        res.json(updatepost);
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const { id } = req.params;
+    const post = req.body;
+    console.log("update request has arrived");
+    const updatepost = await pool.query(
+      "UPDATE posts SET (body) = ($2) WHERE id = $1",
+      [id, post.body]
+    );
+    return res.json(updatepost);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ err: err.message }).send;
+  }
 });
