e9608c928be27de2c87011156fd7fe28149188ed
mammadli <gulnar.mammadli@ut.ee>
2023-12-17 12:39:21 +0200

    ReImplement post CRUD operations

--- back/server.js
+++ back/server.js
@@ -114,6 +114,7 @@ app.post("/posts", async (req, res) => {
   try {
     console.log("a post request has arrived");
     const { body, user_id } = req.body;
+    const comment_count = Math.floor(Math.random() * 20);
     const created_time = new Date();
     const formattedCreatedTime = created_time
       .toISOString()
@@ -126,16 +127,16 @@ app.post("/posts", async (req, res) => {
     const username = name.rows[0].username;
 
     const newpost = await pool.query(
-      "INSERT INTO posts(body, user_id, created_time) values ($1, $2, $3) RETURNING*",
-      [body, user_id, formattedCreatedTime]
+      "INSERT INTO posts(body, user_id, created_time, comment_count) values ($1, $2, $3, $4) RETURNING*",
+      [body, user_id, formattedCreatedTime, comment_count]
     );
 
     const post = newpost.rows[0];
     console.log("new post", newpost);
-    return res.status(201).json({ post: { ...post, username } }).send;
+    return res.status(201).json({ post: { ...post, username } });
   } catch (err) {
     console.error(err.message);
-    return res.status(500).json({ err: err.message }).send;
+    return res.status(500).json({ err: err.message });
   }
 });
 
@@ -170,10 +171,15 @@ app.get("/posts/:id", async (req, res) => {
     console.log("get a post with route parameter  request has arrived");
     const { id } = req.params;
     const posts = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
-    return res.json(posts.rows[0]);
+    if (!posts.rows[0]) {
+      return res
+        .status(404)
+        .json({ err: `This post with id ${id} not found.` });
+    }
+    return res.status(200).json(posts.rows[0]);
   } catch (err) {
     console.error(err.message);
-    return res.status(500).json({ err: err.message }).send;
+    return res.status(500).json({ err: err.message });
   }
 });
 
@@ -181,33 +187,76 @@ app.get("/posts/:id", async (req, res) => {
 app.put("/posts/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const post = req.body;
+    const { body, like_count } = req.body;
     console.log("update request has arrived");
-    const updatepost = await pool.query(
-      "UPDATE posts SET (body) = ($2) WHERE id = $1",
-      [id, post.body]
-    );
-    return res.json(updatepost);
+
+    const post = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
+    if (!post.rows[0]) {
+      return res
+        .status(404)
+        .json({ err: `This post with id ${id} not found.` });
+    }
+
+    let updatepost;
+
+    if (body) {
+      updatepost = await pool.query(
+        "UPDATE posts SET body = $2 WHERE id = $1 RETURNING*",
+        [id, body]
+      );
+    }
+
+    if (like_count) {
+      updatepost = await pool.query(
+        "UPDATE posts SET like_count = $2 WHERE id = $1 RETURNING*",
+        [id, like_count]
+      );
+    }
+    console.log("updatedPost", updatepost);
+    const name = await pool.query("SELECT username FROM users WHERE id = $1", [
+      updatepost.rows[0].user_id,
+    ]);
+    const username = name.rows[0].username;
+    return res.status(200).json({ post: { ...updatepost.rows[0], username } });
   } catch (err) {
     console.error(err.message);
-    return res.status(500).json({ err: err.message }).send;
+    return res.status(500).json({ err: err.message });
   }
 });
-
-// delete All posts
-// user id ()
 
 // delete post
 app.delete("/posts/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    //const post = req.body; // we do not need a body for a delete request
     console.log("delete a post request has arrived");
-    const deletepost = await pool.query("DELETE FROM posts WHERE id = $1", [
-      id,
-    ]);
-    res.json(deletepost);
+    const post = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
+    if (!post.rows[0]) {
+      return res
+        .status(404)
+        .json({ err: `This post with id ${id} not found.` });
+    }
+    await pool.query("DELETE FROM posts WHERE id = $1", [id]);
+    return res
+      .status(200)
+      .json({ msg: `This post with id ${id} successfully deleted.` });
   } catch (err) {
     console.error(err.message);
+    return res.status(500).json({ err: err.message });
+  }
+});
+
+// delete all posts
+app.delete("/posts", async (req, res) => {
+  try {
+    console.log("delete all posts request has arrived");
+    const posts = await pool.query("SELECT * FROM posts");
+    if (!posts.rows[0]) {
+      return res.status(404).json({ err: `No posts found.` });
+    }
+    await pool.query("DELETE FROM posts");
+    return res.status(200).json({ msg: "All posts successfully deleted." });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ err: err.message });
   }
 });
